520ff1bc461a73fd4bfb6e530e5ae63cb795f610
Juan Coutinho <desenvolvedor.juan.business@gmail.com>
2026-02-21 14:04:24 -0300

    fix: Add Ruby 3.4 BigDecimal compatibility, update pg gem constraint, disable bootsnap, and simplify default database connection settings.

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,16 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
-require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+# require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+
+require "logger"
+require "mutex_m"
+require "base64"
+
+# Patch for Ruby 3.4 removing BigDecimal.new
+require 'bigdecimal'
+class BigDecimal
+  def self.new(*args, **kwargs)
+    BigDecimal(*args, **kwargs)
+  end
+end
