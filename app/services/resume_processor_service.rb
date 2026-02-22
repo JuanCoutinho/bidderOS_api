9160906908f1891240d1f5f6480723e2d446b002
Juan Coutinho <desenvolvedor.juan.business@gmail.com>
2026-02-22 20:36:41 -0300

    feat: Remove resume management API and embedding generation from `ResumeProcessorService`.

--- app/services/resume_processor_service.rb
+++ app/services/resume_processor_service.rb
@@ -6,32 +6,21 @@ class ResumeProcessorService
 
     def call
         text = extract_text_from_file
-        raise "Failed to extract text from file" if text.blank?
-    
-        embedding = generate_embedding(text)   
-        
+
         @user.resumes.create!(
             filename: File.basename(@file_path),
             content_text: text,
-            embedding: embedding
+            embedding: nil
         )
     end
 
     private
 
     def extract_text_from_file
-       reader = PDF::Reader.new(@file_path)
-       reader.pages.map(&:text).join("\n")
+        reader = PDF::Reader.new(@file_path)
+        reader.pages.map(&:text).join("\n")
     rescue => e
-        Rails.logger.error("Error extracting text from file: #{e.message}")
-        nil
-    end
-
-    def generate_embedding(text)
-        response = OpenAI::Client.new.embeddings.create(
-            model: "text-embedding-ada-002",
-            input: text
-        )
-        response['data'][0]['embedding']
+        Rails.logger.error("Error extracting text: #{e.message}")
+        ""
     end
 end
