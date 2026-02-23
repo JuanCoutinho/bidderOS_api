63739a9ab56bb9a801c98e6cd81750a4e30e9575
Juan Coutinho <desenvolvedor.juan.business@gmail.com>
2026-02-23 16:36:50 -0300

    feat: add a cors for vercel app

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://bidder-os.vercel.app'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
